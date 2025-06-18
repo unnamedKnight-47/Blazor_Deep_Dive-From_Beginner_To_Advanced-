07ff06c1ec1db7ba778d1fc5e6c043fc96c3aa10
unnamedKnight-47 <atikeprofessional117@gmail.com>
2025-06-18 18:11:56 +0600

    Add Server Model

---
+++ ServerManagement/Components/Models/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace ServerManagement.Components.Models;
+
+public class Server
+{
+    public Server()
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(1, 2);
+        IsOnline = randomNumber != 0;
+    }
+
+    public int ServerId { get; set; }
+    public bool IsOnline { get; set; }
+    public string? Name { get; set; }
+    public string? City { get; set; }
+}
